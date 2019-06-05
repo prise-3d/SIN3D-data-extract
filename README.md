5444926e15adac813bdef9ee1a00ff0032be513b
rigwild <contact@asauvage.fr>
2019-06-05 10:33:35 +0200

    Fixed auto-complete

--- README.md
+++ README.md
@@ -3,36 +3,14 @@
 ## Install
 ```sh
 yarn add https://gogs.univ-littoral.fr/Prise3D/webexpe-data-extract.git
+# or
+npm i git+https://gogs@gogs.univ-littoral.fr/Prise3D/webexpe-data-extract
 ```
 
 ## Usage
-```js
-'use strict'
-
-const path = require('path')
-const { db, utils } = require('webexpe-data-extract')
-
-const setup = async () => {
-  const connection = await db.connect('mongodb://diran.univ-littoral.fr:27017/webexpe')
-
-  const res = await db.Data.findCustom({
-    msgId: 'EXPERIMENT_DATA'
-  })
-
-  console.log(res)
-  console.log(`Found ${res.length} documents matching your request.`)
-
-  const filePath = path.resolve(__dirname, 'searches', `search-${Date.now()}.json`)
-  await utils.outputToFile(res, filePath, true)
-  console.log(`Your search result was saved to ${filePath}`)
-
-  await db.disconnect(connection)
-}
-
-setup()
-```
+See [`/example.js`](./example.js)
 
 ## API
-See [`/lib/db/Data/controller.js#L56-L76`](./lib/db/Data/controller.js#L56-L76).
+See [`/lib/db.js#L83-93`](./lib/db.js#L83-93)
 
 Every functions should be auto-completable in your editor.
--- example.js
+++ example.js
@@ -4,19 +4,27 @@ const path = require('path')
 const { db, utils } = require('./lib')
 
 const setup = async () => {
+  // Connect to the database
   const connection = await db.connect('mongodb://diran.univ-littoral.fr:27017/webexpe')
 
-  const res = await db.Data.findCustom({
+  // Find every documents being experiment data
+  const res = await db.findCustom({
     msgId: 'EXPERIMENT_DATA'
   })
 
+  // Output to console (Do this if you want to pipe to another command!)
   console.log(res)
+
   console.log(`Found ${res.length} documents matching your request.`)
 
+  // Save the results to a JSON file
   const filePath = path.resolve(__dirname, 'searches', `search-${Date.now()}.json`)
-  await utils.outputToFile(res, filePath, true)
-  console.log(`Your search result was saved to ${filePath}`)
 
+  // `res` = search results, `filePath` = output file, `true` = JSON will be pretty-printed (human-readable)
+  await utils.outputToFile(res, filePath, true)
+  console.log(`Your search results were saved to ${filePath}`)
+
+  // Disconnect from the database
   await db.disconnect(connection)
 }
 
